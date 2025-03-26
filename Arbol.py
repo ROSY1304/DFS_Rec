acb0e907a4aaa99b4194db30a9e49110f79a63e2
ROSY1304 <rm007521@gamil.com>
2025-03-26 09:27:08 -0600

    first commit

---
+++ Arbol.py
@@ -0,0 +1,52 @@
+class Nodo:
+    def __init__(self, datos, padre=None):
+        self.datos = datos
+        self.hijos = []  # Inicializa la lista de hijos vacía
+        self.padre = padre
+        self.costo = None
+
+
+    def set_hijos(self, hijos):
+        self.hijos = hijos
+        if self.hijos is not None:
+            for h in hijos:
+                h.padre = self
+
+    def get_hijos(self):
+        return self.hijos
+    
+    def set_datos(self, datos):
+        self.datos = datos
+
+    def get_datos(self):
+        return self.datos
+
+    def get_padre(self):
+        return self.padre
+
+    def set_costo(self, costo):
+        self.costo = costo
+
+    def igual(self, nodo):
+        return self.get_datos() == nodo.get_datos()
+
+    def en_lista(self, lista_nodos):
+        for n in lista_nodos:
+            if self.igual(n):
+                return True
+        return False
+    
+    def get_padre(self):
+        return self.padre
+
+    def set_hijos(self, hijos):
+        self.hijos = hijos
+    
+    def set_padre(self, padre):
+        self.padre = padre
+        
+    def get_hijos(self):
+        return self.hijos
+
+    def __str__(self):
+        return str(self.get_datos())
---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template
+from Arbol import Nodo
+from DFS_prof import DFS_prof_iter
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/solve', methods=['POST'])
+def solve():
+    estado_inicial = list(map(int, request.form['estado_inicial'].split(',')))
+    solucion = list(map(int, request.form['solucion'].split(',')))
+    visitados = []
+    nodo_inicial = Nodo(estado_inicial)
+    nodo = DFS_prof_iter(nodo_inicial, solucion, visitados)
+
+    resultado = []
+    while nodo.get_padre() is not None:
+        resultado.append(nodo.get_datos())
+        nodo = nodo.get_padre()
+    resultado.append(estado_inicial)
+    resultado.reverse()
+
+    return render_template('index.html', resultado=resultado)
+
+if __name__ == '__main__':
+    app.run(debug=True)
